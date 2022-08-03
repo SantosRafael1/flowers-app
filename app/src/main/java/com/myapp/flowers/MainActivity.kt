1afbf6043c7aed166a3e4362fe19263621bf3c5c
Rafael Santos <contactrafaelsantos@gmail.com>
2022-08-03 11:06:10 -0300

    Definindo adapter para o recycler view

--- app/src/main/java/com/myapp/flowers/MainActivity.kt
+++ app/src/main/java/com/myapp/flowers/MainActivity.kt
@@ -2,10 +2,18 @@ package com.myapp.flowers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.myapp.flowers.adapter.ItemAdapter
+import com.myapp.flowers.data.Datasource
+import com.myapp.flowers.model.Flowers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerV = findViewById<RecyclerView>(R.id.recycler_view)
+        val loadData = Datasource().loadData()
+        recyclerV.adapter = ItemAdapter(this, loadData)
     }
 }
